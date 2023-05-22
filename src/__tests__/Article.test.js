761918fcea0e3ac23a057392b0756d0516d3131b
James Sullivan <jsullivan8794@gmail.com>
2023-05-22 12:51:43 -0400

    completed lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,14 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { queryByText, render, screen } from "@testing-library/react";
 import React from "react";
 
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+
+  const element = screen.queryByText('please pass this test');
+  screen.debug(element);
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
